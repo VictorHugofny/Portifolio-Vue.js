7e922a9c3e3f915d55872382598a8953b540c2d9
Victor Hugo <victorhugofny@gmail.com>
2021-10-27 23:39:17 -0300

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,6 +11,133 @@
     </div>
 </article>
 
+<div class="Projetos">
+
+<h1 class="subtitle is-1">Projetos</h1>
+<h1 class = "texto">Abaixos meus <strong> projetos e estudos </strong>, clicando nos links você pode ver uma versão online e o repositorio de cada projeto </h1> 
+<br>
+<div class="columns">
+  <div class="column">
+    <div class="card">
+  <header class="card-header">
+    <p class="card-header-title">
+      Rpg de arena - Vue.js
+    </p>
+   
+  </header>
+  <div class="card-content">
+    <div class="content">
+     <t> Jogo de rpg criado usando Vue.js </t>
+      <br>
+    </div>
+  </div>
+  <footer class="card-footer">
+    <a href="https://rpg-de-arena-vue-js.vercel.app" class="card-footer-item">SITE</a>
+    <a href="https://github.com/VictorHugofny/Rpg-de-Arena-Vue.js" class="card-footer-item">REPOSITORIO</a>
+  </footer>
+
+
+ </div> 
+  </div>
+  <div class="column">
+    <div class="card">
+  <header class="card-header">
+    <p class="card-header-title">
+      Pokedex - Vue.js
+    </p>
+  </header>
+  <div class="card-content">
+    <div class="content">
+      <t> Projeto pokedex usando Vue.js, Axios, PokeAPI </t>
+      <br>
+    </div>
+  </div>
+  <footer class="card-footer">
+    <a href="https://pokedex-vue-js-ten.vercel.app/" class="card-footer-item">SITE</a>
+    <a href="https://github.com/VictorHugofny/Pokedex-Vue.js" class="card-footer-item">REPOSITORIO</a>
+  </footer>
+
+
+ </div> 
+ </div> 
+
+  </div>
+
+<div class="columns">
+  <div class="column">
+    <div class="card">
+  <header class="card-header">
+    <p class="card-header-title">
+      Clones HTML/CSS/JS
+    </p>
+  </header>
+  <div class="card-content">
+    <div class="content">
+      <t> Clones de sites usando HTML/CSS/JS - Sites como Netflix, Disney+, Spotify, Tinder </t>
+      <br>
+    </div>
+  </div>
+  <footer class="card-footer">
+    <a href="https://rpg-de-arena-vue-js.vercel.app" class="card-footer-item">SITE</a>
+    <a href="https://github.com/VictorHugofny/Rpg-de-Arena-Vue.js" class="card-footer-item">REPOSITORIO</a>
+  </footer>
+
+
+ </div> 
+  </div>
+  <div class="column">
+    <div class="card">
+  <header class="card-header">
+    <p class="card-header-title">
+      RPG DE ARENA POO.JS
+    </p>
+  </header>
+  <div class="card">
+    <div class="content">
+      <br>
+      <t> Estudo de POO, criação de um rpg usando objeto </t>
+      <br><br>
+    </div>
+  </div>
+  <footer class="card-footer">
+    <a href="https://victorhugofny.github.io/RPG-DE-ARENA-POO-JS/" class="card-footer-item">SITE</a>
+    <a href="https://github.com/VictorHugofny/RPG-DE-ARENA-POO-JS" class="card-footer-item">REPOSITORIO</a>
+  </footer>
+
+
+ </div> 
+ </div> 
+ 
+  </div>
+
+  <div class="column">
+    <div class="card">
+  <header class="card-header">
+    <p class="card-header-title">
+      Estudos Programacao WEB
+    </p>
+  </header>
+  <div class="card-content">
+    <div class="content">
+      <t> Meus estudos de programação Web  </t>
+      <br>
+    </div>
+  </div>
+  <footer class="card-footer">
+    <a href="https://github.com/VictorHugofny/Programacao-WEB" class="card-footer-item">REPOSITORIO</a>
+  </footer>
+
+
+ </div> 
+ </div> 
+  
+</div>
+    
+<div> 
+<br><br>
+<h1 class="subtitle is-1">Entre em contato</h1>
+<h1 class = "texto">Abaixos <strong> formas de contato </strong>, clicando nos links você sera enviando para cada pagina que deseja </h1>    
+<br>
 <div class = "grid-container" id="rodape"> 
         <div><a href= "mailto:victorhugofny@gmail.com?subject=Hello%20again" target="_blank">
             <h2><img  src="../assets/gmail.png" widht = '10' > </h2></a></div>
@@ -21,9 +148,11 @@
         <div><a href= "https://www.youtube.com/channel/UC0LxIVk-V0k6LsX_Z251iMw" target="_blank">
             <h2> <img src="../assets/youtube.png" >  </h2></a></div>
         <div><a href= "https://www.linkedin.com/in/victor-hugo-santana/" target="_blank">
-            <h2> <img src="../assets/linkedin.png" >  </h2></a></div>
-        
-    </div> </div>
+            <h2> <img src="../assets/linkedin.png" >  </h2></a></div><br><br>
+</div> 
+    
+   </div>  <br><br><br>
+</div>
 
 
 
