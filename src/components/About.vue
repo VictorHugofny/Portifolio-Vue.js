dcf37956f6c29da3f5177e91fd6d40df5dc96941
Victor Hugo <victorhugofny@gmail.com>
2022-01-11 13:47:49 -0300

    Update About.vue

--- src/components/About.vue
+++ src/components/About.vue
@@ -2,7 +2,7 @@
   <div>
     <img class="foto" src="../assets/fotoperfil.jpeg" width="200px"> 
         <h1>Sobre mim</h1>
-        <p> Sou <strong> Victor Hugo</strong>, estudante de Ciencia da computação no IFBaiano, Estudando <strong> programação Web </strong> Frontend usando tecnologias como <strong>Javascript, Html, Css, Vue.js e Node, </strong>  essa pagina foi construida por mim como <strong> portifolio </strong> usando o framework <strong> Vue.js</strong>, faço tambem edições de video como hobby onde atuo como freelancer </p> 
+        <p>Sou <strong> Victor Hugo</strong>, estudante de Ciencia da computação no IFBaiano, Sou <strong> desenvolvedor </strong> uso tecnologias como <strong>Javascript, Html, Css, Vue.js e Node, </strong>  essa pagina foi construida por mim como <strong> portifolio </strong> usando o framework <strong> Vue.js</strong>, faço tambem edições de video como hobby onde atuo como freelancer </p> 
     </div>
 </template>
 
